882c209812527be0a6efbe65662f355600c1314b
Michelle Audirac <audiracmichelle@Michelles-MacBook.local>
2020-06-25 16:48:58 -0500

    first file structure version for modeling

---
+++ plot_foo.R
@@ -0,0 +1,24 @@
+gg_vanilla_sampling <- function(county_data) {
+  county_ <- county_data %>% 
+    filter(index == 1) %>% 
+    pull(county)
+  
+  p <- county_data %>% 
+    ggplot() + 
+    geom_point(aes(x=date, y=deaths)) + 
+    geom_line(aes(x=date, y=fit_med), 
+              col = "blue") + 
+    geom_ribbon(aes(x=date, ymin=fit_lo, ymax=fit_hi), 
+                alpha= 0.1, fill = "blue") + 
+    geom_line(aes(x=date, y=ctr_med), 
+              col = "red") + 
+    geom_ribbon(aes(x=date, ymin=ctr_lo, ymax=ctr_hi), 
+                alpha= 0.1, fill = "red") + 
+    geom_vline(aes(xintercept = stayhome), color = "blue") + 
+    geom_vline(aes(xintercept = stayhome + 12), linetype="dotted", color = "blue") +
+    geom_vline(aes(xintercept = threshold_day + 7), color = "red") + 
+    geom_vline(aes(xintercept = threshold_day + 7 + 12), linetype="dotted", color = "red") + 
+    labs(title = county_, 
+         x = "", y = "")
+  p
+}
