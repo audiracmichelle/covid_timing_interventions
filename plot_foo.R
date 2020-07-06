2d695225c33298656bfa4a702b5f4352861c4f8f
Michelle Audirac <audiracmichelle@Michelles-MacBook.local>
2020-07-06 05:18:23 -0500

    included nchs summary

--- plot_foo.R
+++ plot_foo.R
@@ -22,7 +22,7 @@ gg_intervention_sampling <- function(county_data) {
   p
 }
 
-gg_days_btwn_sampling <- function(county_data) {
+gg_days_btwn_sampling <- function(county_data, up, down) {
   county_ <- county_data %>% 
     filter(index == 1) %>% 
     pull(county)
@@ -44,14 +44,14 @@ gg_days_btwn_sampling <- function(county_data) {
                 alpha= 0.1, fill = "red") +  
     geom_vline(aes(xintercept = stayhome), color = "blue") + 
     geom_vline(aes(xintercept = stayhome + 12), linetype="dotted", color = "blue") + 
-    geom_vline(aes(xintercept = threshold_day + 12 - 7), linetype="dotted", color = "green") + 
-    geom_vline(aes(xintercept = threshold_day + 12 + 5), linetype="dotted", color = "red") + 
+    geom_vline(aes(xintercept = stayhome + 12 + down), linetype="dotted", color = "green") + 
+    geom_vline(aes(xintercept = stayhome + 12 + up), linetype="dotted", color = "red") + 
     labs(title = county_, 
          x = "", y = "")
   p
 }
 
-gg_days_btwn_effect <- function(county_data, up = 7, down = -7) {
+gg_days_btwn_effect <- function(county_data, up, down) {
   county_ <- county_data %>% 
     filter(index == 1) %>% 
     pull(county)
@@ -76,6 +76,32 @@ gg_days_btwn_effect <- function(county_data, up = 7, down = -7) {
     geom_vline(aes(xintercept = stayhome + 12 + down), linetype="dotted", color = "green") + 
     geom_vline(aes(xintercept = stayhome + 12 + up), linetype="dotted", color = "red") + 
     labs(title = county_, 
+         x = "", y = "")
+  p
+}
+
+gg_nchs_sampling <- function(nchs_data, n_, up = up, down = down) {
+  
+  p <- nchs_data %>% 
+    ggplot() + 
+    geom_point(aes(x=days_since_thresh, y=log_y)) + 
+    geom_line(aes(x=date, y=fit_med), 
+              col = "blue") + 
+    geom_ribbon(aes(x=date, ymin=fit_lo, ymax=fit_hi), 
+                alpha= 0.1, fill = "blue") + 
+    geom_line(aes(x=date, y=ctr1_med), 
+              col = "green") + 
+    geom_ribbon(aes(x=date, ymin=ctr1_lo, ymax=ctr1_hi), 
+                alpha= 0.1, fill = "green") + 
+    geom_line(aes(x=date, y=ctr3_med), 
+              col = "red") + 
+    geom_ribbon(aes(x=date, ymin=ctr3_lo, ymax=ctr3_hi), 
+                alpha= 0.1, fill = "red") +  
+    geom_vline(aes(xintercept = stayhome), color = "blue") + 
+    geom_vline(aes(xintercept = stayhome + 12), linetype="dotted", color = "blue") + 
+    geom_vline(aes(xintercept = stayhome + 12 + down), linetype="dotted", color = "green") + 
+    geom_vline(aes(xintercept = stayhome + 12 + up), linetype="dotted", color = "red") + 
+    labs(title = n_, 
          x = "", y = "")
   p
 }
