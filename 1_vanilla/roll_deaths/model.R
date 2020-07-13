c07896f476615202c81470fa5e0de8442ec692d5
Mauricio <mauriciogtec@gmail.com>
2020-07-13 17:34:54 -0500

    ran roll_deaths in 1_vanilla using days_since_intr

--- 1_vanilla/roll_deaths/model.R
+++ 1_vanilla/roll_deaths/model.R
@@ -4,7 +4,7 @@ library(feather)
 library(rstanarm)
 
 ## Read county_train
-county_train <- read_feather("../../county_train_cases.feather")
+county_train <- read_feather("../../county_train.feather")
 
 ## define y
 #length(unique(county_train$fips))
@@ -12,11 +12,12 @@ county_train %<>%
   mutate(y = roll_deaths, 
          intrv_decrease = (date - decrease_40_total_visiting >= 12) * 1, 
          intrv_stayhome = (date - stayhome >= 12) * 1, 
-         days_since_intrv_decrease = as.numeric(date - intrv_decrease), 
-         days_since_intrv_stayhome = as.numeric(date - stayhome)) %>%
+         days_since_intrv_decrease = as.numeric(date - intrv_decrease - 12 + 1), 
+         days_since_intrv_stayhome = as.numeric(date - stayhome - 12 + 1)) %>%
     filter(!is.na(y), 
          !is.na(decrease_40_total_visiting), 
-         !is.na(stayhome))
+         !is.na(stayhome), 
+         days_since_intrv_stayhome <= 17)
 #length(unique(county_train$fips))
 
 # county_train %>%
@@ -29,8 +30,6 @@ model = stan_glmer.nb(
   y ~
     poly(days_since_thresh, 2) + 
     (poly(days_since_thresh, 2) | fips) +
-    days_since_intrv_decrease:intrv_decrease + 
-    I(days_since_intrv_decrease^2):intrv_decrease + 
     days_since_intrv_stayhome:intrv_stayhome + 
     I(days_since_intrv_stayhome^2):intrv_stayhome
   ,
