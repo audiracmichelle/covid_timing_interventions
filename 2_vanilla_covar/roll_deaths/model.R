4036d5ce0d4886e6abab4d351b77e1d1905c40ff
Mauricio <mauriciogtec@gmail.com>
2020-07-14 22:03:08 -0500

    reran transforming new covariates

--- 2_vanilla_covar/roll_deaths/model.R
+++ 2_vanilla_covar/roll_deaths/model.R
@@ -9,19 +9,22 @@ county_train <- read_feather("../../county_train.feather")
 ## define y
 #length(unique(county_train$fips))
 county_train %<>%  
-  mutate(y = roll_deaths, 
-         #intrv_decrease = (date - decrease_40_total_visiting >= 12) * 1, 
-         intrv_stayhome = (date - stayhome >= 12) * 1, 
-         #days_since_intrv_decrease = as.numeric(date - intrv_decrease - 12 + 1), 
-         days_since_intrv_stayhome = as.numeric(date - stayhome - 12 + 1)) %>%
+  mutate(y = roll_deaths,  
+         intrv_stayhome = (date - stayhome >= 12) * 1,  
+         days_since_intrv_stayhome = as.numeric(date - stayhome - 12 + 1), 
+         age_20_44 = log(1e4 * age_20_44 / pop), 
+         age_45_64 = log(1e4 * age_45_64 / pop), 
+         age_65_plus = log(1e4 * age_65_plus / pop), 
+         white = log(1e4 * white / pop), 
+         black = log(1e4 * black / pop), 
+         hispanic = log(1e4 * hispanic / pop)) %>%
     filter(!is.na(y), 
-         #!is.na(decrease_40_total_visiting), 
          !is.na(stayhome), 
          days_since_intrv_stayhome <= 17)
 #length(unique(county_train$fips))
 
 # county_train %>%
-#   select(fips, date, days_since_thresh, intrv_decrease, intrv_stayhome) %>%
+#   select(fips, date, days_since_thresh, intrv_stayhome) %>%
 #   arrange(fips, date) %>%
 #   head(1000) %>% view
 
