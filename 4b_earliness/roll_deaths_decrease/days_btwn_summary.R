c89df601af809897a8b56bf2bb7f4290d816c984
Mauricio <mauriciogtec@gmail.com>
2020-07-20 15:44:55 -0500

    ran 4b for deaths with decrease intervention

---
+++ 4b_earliness/roll_deaths_decrease/days_btwn_summary.R
@@ -0,0 +1,104 @@
+library(tidyverse)
+library(magrittr)
+library(feather)
+library(rstanarm)
+library(gridExtra)
+
+## Read data
+county_pred <- read_feather("../../county_train.feather")
+model <- readRDS("./model.rds")
+county_fit <- readRDS("./county_fit.rds")
+source("../../plot_foo.R")
+
+up = 7
+down = -14
+
+## define y
+#length(unique(county_pred$fips))
+county_pred %<>%  
+  mutate(y = roll_deaths,  
+         #intrv_stayhome = (date - stayhome >= 12) * 1,  
+         intrv_decrease = (date - decrease_50_total_visiting >= 12) * 1,
+         #days_since_intrv_stayhome = as.numeric(date - stayhome - 12 + 1), 
+         days_since_intrv_decrease = as.numeric(date - decrease_50_total_visiting - 12 + 1),
+         age_45_64 = log(1e4 * age_45_64 / pop), 
+         age_65_plus = log(1e4 * age_65_plus / pop), 
+         black = log(1e4 * black / pop), 
+         hispanic = log(1e4 * hispanic / pop), 
+         days_btwn_decrease_thresh = as.numeric(decrease_50_total_visiting - threshold_day)
+         ) %>%
+    filter(!is.na(y), 
+         #!is.na(stayhome), 
+         !is.na(decrease_50_total_visiting), 
+         #days_since_intrv_stayhome <= 17
+         days_since_intrv_decrease <= 17
+         )
+#length(unique(county_pred$fips))
+
+## obtain distribution values from fit sampling
+county_pred %<>% 
+  mutate(
+    fit_mu = apply(county_fit, 2, mean),
+    fit_med = apply(county_fit, 2, quantile, probs = 0.5), # use posterior median to hand skewness
+    fit_lo = apply(county_fit, 2, quantile, probs = 0.05),
+    fit_hi = apply(county_fit, 2, quantile, probs = 0.95))
+
+## modify values to obtain counterfactual
+county_pred1 = county_pred %>% 
+  mutate(days_btwn_decrease_thresh = days_btwn_decrease_thresh + up) %>%
+  mutate(intrv_decrease = as.numeric(date >= decrease_50_total_visiting + 12 + up))
+
+county_pred3 = county_pred %>% 
+  mutate(days_btwn_decrease_thresh = days_btwn_decrease_thresh + down) %>%
+  mutate(intrv_decrease = as.numeric(date >= decrease_50_total_visiting + 12 + down))
+
+# not sure what should go here
+county_pred1$days_since_intrv_decrease <- county_pred1$days_since_intrv_decrease - up
+county_pred3$days_since_intrv_decrease <- county_pred3$days_since_intrv_decrease - down
+
+## get posteriors
+county_ctr1 <- model %>% 
+  posterior_predict(county_pred1, draws = 500)
+county_ctr3 <- model %>% 
+  posterior_predict(county_pred3, draws = 500)
+
+## generate nchs summaries
+
+county_pred %<>% 
+  mutate(
+    ctr1_mu = apply(county_ctr1, 2, mean),
+    ctr1_med = apply(county_ctr1, 2, quantile, probs = 0.5), # use posterior median to hand skewness
+    ctr1_lo = apply(county_ctr1, 2, quantile, probs = 0.05),
+    ctr1_hi = apply(county_ctr1, 2, quantile, probs = 0.95),
+    ctr3_mu = apply(county_ctr3, 2, mean),
+    ctr3_med = apply(county_ctr3, 2, quantile, probs = 0.5), # use posterior median to hand skewness
+    ctr3_lo = apply(county_ctr3, 2, quantile, probs = 0.05),
+    ctr3_hi = apply(county_ctr3, 2, quantile, probs = 0.95))
+
+for(c in 1:6) {
+    fips_ <- county_pred %>% 
+      distinct(fips, nchs, pop) %>% 
+      filter(nchs == c) %>% 
+      arrange(desc(pop)) %>% 
+      pull(fips)
+
+    name_ <- county_pred %>%
+      filter(fips %in% fips_) %>% 
+      distinct(fips, state, county) %>%
+      mutate(name = paste(state, county))
+
+  county_plots <- lapply(fips_, 
+                         function(x) county_pred %>% 
+                           filter(fips == x) %>% 
+                           gg_days_btwn_sampling(
+                           name = name_$name[name_$fips == x], 
+                           up = up, 
+                           down = down, 
+                           lag = 12))
+  county_plots <- marrangeGrob(county_plots, 
+                               nrow = 6, ncol = 2, 
+                               left = "", top = "")
+  ggsave(paste("./days_btwn_summary/", 
+               "sampling_nchs_", c, ".pdf", sep = ""), 
+         county_plots, width = 15, height = 25, units = "cm")
+}
---
+++ 4b_earliness/roll_deaths_decrease/model.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(magrittr)
+library(feather)
+library(rstanarm)
+
+## Read county_train
+county_train <- read_feather("../../county_train.feather")
+
+## define y
+#length(unique(county_train$fips))
+county_train %<>%  
+  mutate(y = roll_deaths,  
+         #intrv_stayhome = (date - stayhome >= 12) * 1,  
+         intrv_decrease = (date - decrease_50_total_visiting >= 12) * 1,
+         #days_since_intrv_stayhome = as.numeric(date - stayhome - 12 + 1), 
+         days_since_intrv_decrease = as.numeric(date - decrease_50_total_visiting - 12 + 1),
+         age_45_64 = log(1e4 * age_45_64 / pop), 
+         age_65_plus = log(1e4 * age_65_plus / pop), 
+         black = log(1e4 * black / pop), 
+         hispanic = log(1e4 * hispanic / pop), 
+         days_btwn_decrease_thresh = as.numeric(decrease_50_total_visiting - threshold_day)
+         ) %>%
+    filter(!is.na(y), 
+         #!is.na(stayhome), 
+         !is.na(decrease_50_total_visiting), 
+         #days_since_intrv_stayhome <= 17
+         days_since_intrv_decrease <= 17
+         )
+#length(unique(county_train$fips))
+
+# county_train %>%
+#   select(fips, date, days_since_thresh, intrv_stayhome) %>%
+#   arrange(fips, date) %>%
+#   head(1000) %>% view
+
+## Train model
+model = stan_glmer.nb(
+  y ~
+    poly(days_since_thresh, 2) * (nchs + college + age_65_plus + black + hispanic) + 
+    (poly(days_since_thresh, 2) | fips) +
+    days_since_intrv_decrease:intrv_decrease + 
+    I(days_since_intrv_decrease^2):intrv_decrease + 
+    days_since_intrv_decrease:intrv_decrease:days_btwn_decrease_thresh +
+    I(days_since_intrv_decrease^2):intrv_decrease:days_btwn_decrease_thresh + 
+    days_since_intrv_decrease:intrv_decrease:nchs +
+    I(days_since_intrv_decrease^2):intrv_decrease:nchs 
+  ,
+  offset = log(pop),
+  data=county_train,
+  algorithm="meanfield",
+  iter = 50000,
+  adapt_iter = 2500,
+  QR=TRUE
+)
+
+saveRDS(model, paste("./model.rds", sep = ""))
+
+#### #### 
+## county_fit
+
+county_fit <- model %>%
+  posterior_predict(county_train, draws = 500)
+
+saveRDS(county_fit, "./county_fit.rds")
