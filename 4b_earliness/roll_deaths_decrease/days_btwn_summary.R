dd416e93c6af0f47c637b13eb6a779740e58fb0d
Mauricio <mauriciogtec@gmail.com>
2020-07-21 15:17:14 -0500

    included intervention summary and corrected vertical lines in days_btwn summary

--- 4b_earliness/roll_deaths_decrease/days_btwn_summary.R
+++ 4b_earliness/roll_deaths_decrease/days_btwn_summary.R
@@ -94,7 +94,7 @@ for(c in 1:6) {
                            name = name_$name[name_$fips == x], 
                            up = up, 
                            down = down, 
-                           lag = 12))
+                           lag_decrease = 12))
   county_plots <- marrangeGrob(county_plots, 
                                nrow = 6, ncol = 2, 
                                left = "", top = "")
@@ -102,3 +102,68 @@ for(c in 1:6) {
                "sampling_nchs_", c, ".pdf", sep = ""), 
          county_plots, width = 15, height = 25, units = "cm")
 }
+
+# ## generate cumulative effect summary
+# county_fit_effect <- matrix(nrow = 500, ncol = 0)
+# county_ctr1_effect <- matrix(nrow = 500, ncol = 0)
+# county_ctr3_effect <- matrix(nrow = 500, ncol = 0)
+# for(f in unique(county_pred$fips)){
+#   print(f)
+#   county_idx <- which(county_pred$fips == f)
+#   fit <- county_fit[, county_idx]
+#   fit <- t(apply(fit, 1, cumsum))
+#   ctr1 <- county_ctr1[, county_idx]
+#   ctr1 <- t(apply(ctr1, 1, cumsum))
+#   ctr3 <- county_ctr3[, county_idx]
+#   ctr3 <- t(apply(ctr3, 1, cumsum))
+  
+#   county_fit_effect <- cbind(county_fit_effect, fit)
+#   county_ctr1_effect <- cbind(county_ctr1_effect, ctr1)
+#   county_ctr3_effect <- cbind(county_ctr3_effect, ctr3) 
+# }
+
+# county_pred %<>% 
+#   mutate(
+#     fit_mu = apply(county_fit_effect, 2, mean),
+#     fit_med = apply(county_fit_effect, 2, quantile, probs = 0.5), # use posterior median to hand skewness
+#     fit_lo = apply(county_fit_effect, 2, quantile, probs = 0.05),
+#     fit_hi = apply(county_fit_effect, 2, quantile, probs = 0.95))
+
+# county_pred %<>% 
+#   mutate(
+#     ctr1_mu = apply(county_ctr1_effect, 2, mean),
+#     ctr1_med = apply(county_ctr1_effect, 2, quantile, probs = 0.5), # use posterior median to hand skewness
+#     ctr1_lo = apply(county_ctr1_effect, 2, quantile, probs = 0.05),
+#     ctr1_hi = apply(county_ctr1_effect, 2, quantile, probs = 0.95),
+#     ctr3_mu = apply(county_ctr3_effect, 2, mean),
+#     ctr3_med = apply(county_ctr3_effect, 2, quantile, probs = 0.5), # use posterior median to hand skewness
+#     ctr3_lo = apply(county_ctr3_effect, 2, quantile, probs = 0.05),
+#     ctr3_hi = apply(county_ctr3_effect, 2, quantile, probs = 0.95))
+
+# for(c in 1:6) {
+#   fips_ <- county_pred %>% 
+#     distinct(fips, nchs, pop) %>% 
+#     filter(nchs == c) %>% 
+#     arrange(desc(pop)) %>% 
+#     pull(fips)
+  
+#   name_ <- county_pred %>%
+#     filter(fips %in% fips_) %>% 
+#     distinct(fips, state, county) %>%
+#     mutate(name = paste(state, county))
+  
+#   county_plots <- lapply(fips_, 
+#                          function(x) county_pred %>% 
+#                            filter(fips == x) %>% 
+#                            gg_days_btwn_sampling(
+#                              name = name_$name[name_$fips == x], 
+#                              up = up, 
+#                              down = down, 
+#                              lag_decrease = 12))
+#   county_plots <- marrangeGrob(county_plots, 
+#                                nrow = 6, ncol = 2, 
+#                                left = "", top = "")
+#   ggsave(paste("./days_btwn_summary/", 
+#                "effect_nchs_", c, ".pdf", sep = ""), 
+#          county_plots, width = 15, height = 25, units = "cm")
+# }
